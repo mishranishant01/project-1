af87a7d313b91ce8e2e4a136f9d7d87343374db6
Nishant <nishant.c.11924@iibm.in>
2024-07-18 14:15:19 +0530

    Adding React Code

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const HeroSection = () => {
+  return (
+
+    <main className="hero">
+
+      <div className="hero-content">
+
+
+        <h1> YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BETTER AND WE ARE HRE TO HELP  YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU</p>
+    
+        <div className="hero-btn"> 
+        <button>Shop Now</button>
+        <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="Shopping">
+          <p>Also Available on Amazon</p>
+        </div>
+      </div>
+      
+      <div className="hero-image">
+        <img src="download.jpg" alt="hero-image" />
+      </div>
+    </main>
+  );
+
+};
+
+export default HeroSection;
